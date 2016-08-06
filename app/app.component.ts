e08c3ed3ffd69cf9600e722921f60232d5fffe87
shuyucao <shuyucao@microsoft.com>
2016-08-05 20:30:22 -0700

    horizontal layout

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <div class="pane"></div>
+        <div class="content">
+            <div class="canvas">
+                <div *ngFor="let value of values"
+                     class="tile"
+                     [ngClass]="'size' + value % 3">
+                </div>
+            </div>
+        </div>
+    `,
+    styles: [`
+        :host {
+            height: 100%;
+            width: 100%;
+            display: flex;
+        }
+        .pane {
+            background-color: #E2E2E2;
+            width: 250px;
+            flex-shrink: 0;
+        }
+        .content {
+            flex-grow: 1;
+            overflow-x: scroll;
+            position: relative;
+        }
+        .canvas {
+            flex-flow: column wrap;
+            display: flex;
+            align-content: flex-start;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            padding: 10px;
+        }
+        .tile {
+            width: 200px;
+            background-color: #B5B5B5;
+            margin: 10px;
+        }
+        .size0 {
+            height: 200px;
+        }
+        .size1 {
+            height: 300px;
+        }
+        .size2 {
+            height: 400px;
+        }
+    `]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    values: number[];
+
+    ngOnInit(): void {
+        this.values = [];
+        for (let i = 0; i < 20; i++) {
+            this.values.push(Math.floor(Math.random() * 10));
+        }
+    }
+}
