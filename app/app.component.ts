e6a8017978a5db2fa95469c651ad673e52ad4c90
shuyucao <shuyucao@microsoft.com>
2016-08-05 21:35:13 -0700

    try calc column

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div class="pane"></div>
+        <div class="pane"
+            (click)="clickPane($event)">
+            <span>click to switch between horizontal and vertical layout</span>
+            <span>ctrl+click to refresh</span>
+        </div>
         <div class="content">
             <div *ngIf="horizontal" class="horizontal-canvas">
                 <div *ngFor="let value of values"
                      class="tile"
                      [style.height.px]="value">
+                     height: {{value}}
                 </div>
             </div>
-            <div *ngIf="!horizontal" class="vertical-canvas">
-                <div *ngFor="let valueGroup of valueGroups" class="group">
-                    <div *ngFor="let value of valueGroup"
-                        class="tile"
-                        [style.height.px]="value">
-                    </div>
+            <div *ngIf="!horizontal"
+                 class="vertical-canvas"
+                 [style.height.px]="canvasHeight">
+                <div *ngFor="let value of values"
+                    class="tile"
+                    [style.height.px]="value">
+                    height: {{value}}
                 </div>
             </div>
         </div>
@@ -28,20 +34,24 @@ import { Component, OnInit } from '@angular/core';
             display: flex;
         }
         .pane {
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+            justify-content: space-around;
             background-color: #E2E2E2;
             width: 250px;
             flex-shrink: 0;
+            padding: 10px;
         }
         .content {
             flex-grow: 1;
-            overflow-x: scroll;
+            overflow: auto;
             position: relative;
         }
 
         .horizontal-canvas {
             display: flex;
             flex-flow: column wrap;
-            align-content: flex-start;
             position: absolute;
             top: 0;
             bottom: 0;
@@ -52,57 +62,82 @@ import { Component, OnInit } from '@angular/core';
         .vertical-canvas {
             width: 100%;
             display: flex;
-            flex-flow: row wrap;
+            flex-flow: column wrap;
         }
 
         .tile {
             background-color: #B5B5B5;
-            margin: 10px;
+            margin-bottom: 10px;
+            margin-left: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
         .horizontal-canvas .tile {
             width: 200px;
         }
-        .vertical-canvas .group {
+        .vertical-canvas .tile {
             width: 30%;
-            min-width: 150px;
         }
     `]
 })
 export class AppComponent implements OnInit {
     private _valueCount = 20;
-    private _groupCount = 3;
+    private _columnCount = 3;
+    private _tileMargin = 10;
 
     horizontal: boolean;
     values: number[];
-    valueGroups: number[][];
+    canvasHeight: number;
 
     ngOnInit(): void {
-        this.horizontal = false;
+        this.horizontal = true;
 
         this.values = [];
         for (let i = 0; i < this._valueCount; i++) {
             this.values.push(Math.floor(100 + Math.random() * 300));
         }
 
-        let groupSum: number[] = [];
-        this.valueGroups = [];
-        for (let i = 0; i < this._groupCount; i++) {
-            this.valueGroups.push([]);
-            groupSum.push(0);
-        }
-
-        for (let i = 0; i < this.values.length; i++) {
-            let minSum = Number.MAX_SAFE_INTEGER;
-            let index: number;
-            for (let i = 0; i < this._groupCount; i++) {
-                if (groupSum[i] < minSum) {
-                    minSum = groupSum[i];
-                    index = i;
+        let totalHeight = this.values.reduce((x, y) => x + y + this._tileMargin*2);
+        let columnHeight = totalHeight / this._columnCount;;
+        while (true) {
+            let column = 0;
+            let remain = columnHeight;
+            let fistUnfitNeed: number;
+            let fistUnfitRemain: number;
+            for (let i = 0; i < this._valueCount;) {
+                let need = this.values[i] + this._tileMargin;
+                if (remain - need >= 0) {
+                    remain -= need;
+                    i++;
+                } else {
+                    if (column === 0) {
+                        fistUnfitNeed = need;
+                        fistUnfitRemain = remain;
+                    }
+                    column++;
+                    remain = columnHeight;
+                    if (column >= this._columnCount) {
+                        break;
+                    }
                 }
             }
 
-            this.valueGroups[index].push(this.values[i]);
-            groupSum[index] += this.values[i];
+            if (column < this._columnCount) {
+                break;
+            } else {
+                columnHeight += (fistUnfitNeed - fistUnfitRemain);
+            }
+        }
+
+        this.canvasHeight = columnHeight;
+    }
+
+    clickPane(event: MouseEvent): void {
+        if (!event.ctrlKey) {
+            this.horizontal = !this.horizontal;
+        } else {
+            this.ngOnInit();
         }
     }
 }
