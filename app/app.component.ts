1432beb11ea21f30aab2b2db806bcd3aabe334b6
shuyucao <shuyucao@microsoft.com>
2016-08-05 20:55:46 -0700

    try group

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,18 @@ import { Component, OnInit } from '@angular/core';
     template: `
         <div class="pane"></div>
         <div class="content">
-            <div class="canvas">
+            <div *ngIf="horizontal" class="horizontal-canvas">
                 <div *ngFor="let value of values"
                      class="tile"
-                     [ngClass]="'size' + value % 3">
+                     [style.height.px]="value">
+                </div>
+            </div>
+            <div *ngIf="!horizontal" class="vertical-canvas">
+                <div *ngFor="let valueGroup of valueGroups" class="group">
+                    <div *ngFor="let value of valueGroup"
+                        class="tile"
+                        [style.height.px]="value">
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,9 +37,10 @@ import { Component, OnInit } from '@angular/core';
             overflow-x: scroll;
             position: relative;
         }
-        .canvas {
-            flex-flow: column wrap;
+
+        .horizontal-canvas {
             display: flex;
+            flex-flow: column wrap;
             align-content: flex-start;
             position: absolute;
             top: 0;
@@ -40,29 +49,60 @@ import { Component, OnInit } from '@angular/core';
             right: 0;
             padding: 10px;
         }
+        .vertical-canvas {
+            width: 100%;
+            display: flex;
+            flex-flow: row wrap;
+        }
+
         .tile {
-            width: 200px;
             background-color: #B5B5B5;
             margin: 10px;
         }
-        .size0 {
-            height: 200px;
+        .horizontal-canvas .tile {
+            width: 200px;
         }
-        .size1 {
-            height: 300px;
-        }
-        .size2 {
-            height: 400px;
+        .vertical-canvas .group {
+            width: 30%;
+            min-width: 150px;
         }
     `]
 })
 export class AppComponent implements OnInit {
+    private _valueCount = 20;
+    private _groupCount = 3;
+
+    horizontal: boolean;
     values: number[];
+    valueGroups: number[][];
 
     ngOnInit(): void {
+        this.horizontal = false;
+
         this.values = [];
-        for (let i = 0; i < 20; i++) {
-            this.values.push(Math.floor(Math.random() * 10));
+        for (let i = 0; i < this._valueCount; i++) {
+            this.values.push(Math.floor(100 + Math.random() * 300));
+        }
+
+        let groupSum: number[] = [];
+        this.valueGroups = [];
+        for (let i = 0; i < this._groupCount; i++) {
+            this.valueGroups.push([]);
+            groupSum.push(0);
+        }
+
+        for (let i = 0; i < this.values.length; i++) {
+            let minSum = Number.MAX_SAFE_INTEGER;
+            let index: number;
+            for (let i = 0; i < this._groupCount; i++) {
+                if (groupSum[i] < minSum) {
+                    minSum = groupSum[i];
+                    index = i;
+                }
+            }
+
+            this.valueGroups[index].push(this.values[i]);
+            groupSum[index] += this.values[i];
         }
     }
 }
